c295780e29720cae6bc62eef8035883f85a8dcfc
Venkat R <venkat.ravikanti@caci.com>
2018-08-16 14:32:53 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# devesssentials
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: devesssentials
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
